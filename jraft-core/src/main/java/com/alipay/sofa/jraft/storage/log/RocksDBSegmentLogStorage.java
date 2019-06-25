712765fb4ee0b8e2fd01ef6db02a0953cb1433d9
boyan <boyan@antfin.com>
2019-06-25 21:12:35 +0800

    (feat) minor changes in RocksDBSegmentLogStorage#onShutdown

--- jraft-core/src/main/java/com/alipay/sofa/jraft/storage/log/RocksDBSegmentLogStorage.java
+++ jraft-core/src/main/java/com/alipay/sofa/jraft/storage/log/RocksDBSegmentLogStorage.java
@@ -21,6 +21,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
@@ -303,19 +304,21 @@ public class RocksDBSegmentLogStorage extends RocksDBLogStorage {
 
     @Override
     protected void onShutdown() {
+        stopCheckpointTask();
+        List<SegmentFile> shutdownFiles = Collections.emptyList();
         this.writeLock.lock();
         try {
-            stopCheckpointTask();
             doCheckpoint();
-            for (final SegmentFile segmentFile : this.segments) {
-                segmentFile.shutdown();
-            }
+            shutdownFiles = new ArrayList<>(shutdownFiles);
             this.segments.clear();
             if (!this.abortFile.destroy()) {
                 LOG.error("Fail to delete abort file {}.", this.abortFile.getPath());
             }
         } finally {
             this.writeLock.unlock();
+            for (final SegmentFile segmentFile : shutdownFiles) {
+                segmentFile.shutdown();
+            }
         }
     }
 
