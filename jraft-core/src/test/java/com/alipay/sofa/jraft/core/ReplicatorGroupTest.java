a8db8263d047aaf75c9c6d165cee65a193e0c301
jiachun.fjc <jiachun_fjc@163.com>
2019-08-17 13:49:40 +0800

    (fix) refactor unit test

--- jraft-core/src/test/java/com/alipay/sofa/jraft/core/ReplicatorGroupTest.java
+++ jraft-core/src/test/java/com/alipay/sofa/jraft/core/ReplicatorGroupTest.java
@@ -16,6 +16,14 @@
  */
 package com.alipay.sofa.jraft.core;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+
 import com.alipay.sofa.jraft.ReplicatorGroup;
 import com.alipay.sofa.jraft.entity.NodeId;
 import com.alipay.sofa.jraft.entity.PeerId;
@@ -27,20 +35,6 @@ import com.alipay.sofa.jraft.rpc.RpcRequests;
 import com.alipay.sofa.jraft.rpc.impl.FutureImpl;
 import com.alipay.sofa.jraft.storage.LogManager;
 import com.alipay.sofa.jraft.storage.SnapshotStorage;
-import com.alipay.sofa.jraft.test.TestUtils;
-import com.alipay.sofa.jraft.util.Endpoint;
-import java.util.ArrayList;
-import java.util.List;
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.runners.MockitoJUnitRunner;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
@@ -50,38 +44,37 @@ import static org.mockito.Matchers.eq;
 @RunWith(value = MockitoJUnitRunner.class)
 public class ReplicatorGroupTest {
 
-    private TimerManager           timerManager;
-    private ReplicatorGroup        replicatorGroup;
+    private TimerManager      timerManager;
+    private ReplicatorGroup   replicatorGroup;
     @Mock
-    private BallotBox              ballotBox;
+    private BallotBox         ballotBox;
     @Mock
-    private LogManager             logManager;
+    private LogManager        logManager;
     @Mock
-    private NodeImpl               node;
-    private ReplicatorGroupOptions rgOpts;
+    private NodeImpl          node;
     @Mock
-    private RaftClientService      rpcService;
+    private RaftClientService rpcService;
     @Mock
-    private SnapshotStorage        snapshotStorage;
-    private NodeOptions            options     = new NodeOptions();
-    private final RaftOptions      raftOptions = new RaftOptions();
-    private final PeerId           peerId1     = new PeerId("localhost", 8082);
-    private final PeerId           peerId2     = new PeerId("localhost", 8083);
-    private final PeerId           peerId3     = new PeerId("localhost", 8084);
+    private SnapshotStorage   snapshotStorage;
+    private NodeOptions       options     = new NodeOptions();
+    private final RaftOptions raftOptions = new RaftOptions();
+    private final PeerId      peerId1     = new PeerId("localhost", 8082);
+    private final PeerId      peerId2     = new PeerId("localhost", 8083);
+    private final PeerId      peerId3     = new PeerId("localhost", 8084);
 
     @Before
     public void setup() {
         this.timerManager = new TimerManager();
         this.timerManager.init(5);
-        replicatorGroup = new ReplicatorGroupImpl();
-        rgOpts = new ReplicatorGroupOptions();
+        this.replicatorGroup = new ReplicatorGroupImpl();
+        ReplicatorGroupOptions rgOpts = new ReplicatorGroupOptions();
         rgOpts.setHeartbeatTimeoutMs(heartbeatTimeout(this.options.getElectionTimeoutMs()));
         rgOpts.setElectionTimeoutMs(this.options.getElectionTimeoutMs());
         rgOpts.setLogManager(this.logManager);
         rgOpts.setBallotBox(this.ballotBox);
-        rgOpts.setNode(node);
+        rgOpts.setNode(this.node);
         rgOpts.setRaftRpcClientService(this.rpcService);
-        rgOpts.setSnapshotStorage(snapshotStorage);
+        rgOpts.setSnapshotStorage(this.snapshotStorage);
         rgOpts.setRaftOptions(this.raftOptions);
         rgOpts.setTimerManager(this.timerManager);
         Mockito.when(this.logManager.getLastLogIndex()).thenReturn(10L);
@@ -89,60 +82,60 @@ public class ReplicatorGroupTest {
         Mockito.when(this.node.getNodeMetrics()).thenReturn(new NodeMetrics(false));
         Mockito.when(this.node.getNodeId()).thenReturn(new NodeId("test", new PeerId("localhost", 8081)));
         mockSendEmptyEntries();
-        assertTrue(this.replicatorGroup.init(node.getNodeId(), rgOpts));
+        assertTrue(this.replicatorGroup.init(this.node.getNodeId(), rgOpts));
     }
 
     @Test
     public void testAddReplicatorAndFailed() {
-        replicatorGroup.resetTerm(1);
-        assertFalse(replicatorGroup.addReplicator(peerId1));
+        this.replicatorGroup.resetTerm(1);
+        assertFalse(replicatorGroup.addReplicator(this.peerId1));
     }
 
     @Test
     public void testAddReplicatorSuccess() {
-        Mockito.when(this.rpcService.connect(peerId1.getEndpoint())).thenReturn(true);
-        replicatorGroup.resetTerm(1);
-        assertTrue(replicatorGroup.addReplicator(peerId1));
+        Mockito.when(this.rpcService.connect(this.peerId1.getEndpoint())).thenReturn(true);
+        this.replicatorGroup.resetTerm(1);
+        assertTrue(this.replicatorGroup.addReplicator(this.peerId1));
     }
 
     @Test
     public void testStopReplicator() {
-        Mockito.when(this.rpcService.connect(peerId1.getEndpoint())).thenReturn(true);
-        replicatorGroup.resetTerm(1);
-        replicatorGroup.addReplicator(peerId1);
-        assertTrue(replicatorGroup.stopReplicator(peerId1));
+        Mockito.when(this.rpcService.connect(this.peerId1.getEndpoint())).thenReturn(true);
+        this.replicatorGroup.resetTerm(1);
+        this.replicatorGroup.addReplicator(this.peerId1);
+        assertTrue(replicatorGroup.stopReplicator(this.peerId1));
     }
 
     @Test
     public void testStopAllReplicator() {
-        Mockito.when(this.rpcService.connect(peerId1.getEndpoint())).thenReturn(true);
-        Mockito.when(this.rpcService.connect(peerId2.getEndpoint())).thenReturn(true);
-        Mockito.when(this.rpcService.connect(peerId3.getEndpoint())).thenReturn(true);
-        replicatorGroup.resetTerm(1);
-        replicatorGroup.addReplicator(peerId1);
-        replicatorGroup.addReplicator(peerId2);
-        replicatorGroup.addReplicator(peerId3);
-        assertTrue(replicatorGroup.contains(peerId1));
-        assertTrue(replicatorGroup.contains(peerId2));
-        assertTrue(replicatorGroup.contains(peerId3));
-        assertTrue(replicatorGroup.stopAll());
+        Mockito.when(this.rpcService.connect(this.peerId1.getEndpoint())).thenReturn(true);
+        Mockito.when(this.rpcService.connect(this.peerId2.getEndpoint())).thenReturn(true);
+        Mockito.when(this.rpcService.connect(this.peerId3.getEndpoint())).thenReturn(true);
+        this.replicatorGroup.resetTerm(1);
+        this.replicatorGroup.addReplicator(this.peerId1);
+        this.replicatorGroup.addReplicator(this.peerId2);
+        this.replicatorGroup.addReplicator(this.peerId3);
+        assertTrue(this.replicatorGroup.contains(this.peerId1));
+        assertTrue(this.replicatorGroup.contains(this.peerId2));
+        assertTrue(this.replicatorGroup.contains(this.peerId3));
+        assertTrue(this.replicatorGroup.stopAll());
     }
 
     @Test
     public void testTransferLeadershipToAndStop() {
-        Mockito.when(this.rpcService.connect(peerId1.getEndpoint())).thenReturn(true);
-        Mockito.when(this.rpcService.connect(peerId2.getEndpoint())).thenReturn(true);
-        Mockito.when(this.rpcService.connect(peerId3.getEndpoint())).thenReturn(true);
-        replicatorGroup.resetTerm(1);
-        replicatorGroup.addReplicator(peerId1);
-        replicatorGroup.addReplicator(peerId2);
-        replicatorGroup.addReplicator(peerId3);
-        long logIndx = 8;
-        assertTrue(replicatorGroup.transferLeadershipTo(peerId1, 8));
-        Replicator r = (Replicator) replicatorGroup.getReplicator(peerId1).lock();
-        assertEquals(r.getTimeoutNowIndex(), logIndx);
-        replicatorGroup.getReplicator(peerId1).unlock();
-        assertTrue(replicatorGroup.stopTransferLeadership(peerId1));
+        Mockito.when(this.rpcService.connect(this.peerId1.getEndpoint())).thenReturn(true);
+        Mockito.when(this.rpcService.connect(this.peerId2.getEndpoint())).thenReturn(true);
+        Mockito.when(this.rpcService.connect(this.peerId3.getEndpoint())).thenReturn(true);
+        this.replicatorGroup.resetTerm(1);
+        this.replicatorGroup.addReplicator(this.peerId1);
+        this.replicatorGroup.addReplicator(this.peerId2);
+        this.replicatorGroup.addReplicator(this.peerId3);
+        long logIndex = 8;
+        assertTrue(this.replicatorGroup.transferLeadershipTo(this.peerId1, 8));
+        Replicator r = (Replicator) this.replicatorGroup.getReplicator(this.peerId1).lock();
+        assertEquals(r.getTimeoutNowIndex(), logIndex);
+        this.replicatorGroup.getReplicator(this.peerId1).unlock();
+        assertTrue(this.replicatorGroup.stopTransferLeadership(this.peerId1));
         assertEquals(r.getTimeoutNowIndex(), 0);
     }
 
@@ -156,51 +149,30 @@ public class ReplicatorGroupTest {
     }
 
     private void mockSendEmptyEntries() {
-        final RpcRequests.AppendEntriesRequest request1 = createEmptyEntriesRequestToPeer1();
-        final RpcRequests.AppendEntriesRequest request2 = createEmptyEntriesRequestToPeer2();
-        final RpcRequests.AppendEntriesRequest request3 = createEmptyEntriesRequestToPeer3();
+        final RpcRequests.AppendEntriesRequest request1 = createEmptyEntriesRequestToPeer(this.peerId1);
+        final RpcRequests.AppendEntriesRequest request2 = createEmptyEntriesRequestToPeer(this.peerId2);
+        final RpcRequests.AppendEntriesRequest request3 = createEmptyEntriesRequestToPeer(this.peerId3);
 
-        Mockito.when(this.rpcService.appendEntries(eq(peerId1.getEndpoint()), eq(request1), eq(-1), Mockito.any()))
+        Mockito
+            .when(this.rpcService.appendEntries(eq(this.peerId1.getEndpoint()), eq(request1), eq(-1), Mockito.any()))
             .thenReturn(new FutureImpl<>());
-        Mockito.when(this.rpcService.appendEntries(eq(peerId2.getEndpoint()), eq(request2), eq(-1), Mockito.any()))
+        Mockito
+            .when(this.rpcService.appendEntries(eq(this.peerId2.getEndpoint()), eq(request2), eq(-1), Mockito.any()))
             .thenReturn(new FutureImpl<>());
-        Mockito.when(this.rpcService.appendEntries(eq(peerId3.getEndpoint()), eq(request3), eq(-1), Mockito.any()))
+        Mockito
+            .when(this.rpcService.appendEntries(eq(this.peerId3.getEndpoint()), eq(request3), eq(-1), Mockito.any()))
             .thenReturn(new FutureImpl<>());
     }
 
-    private RpcRequests.AppendEntriesRequest createEmptyEntriesRequestToPeer1() {
-        final RpcRequests.AppendEntriesRequest request = RpcRequests.AppendEntriesRequest.newBuilder(). //
-            setGroupId("test"). //
-            setServerId(new PeerId("localhost", 8081).toString()). //
-            setPeerId(this.peerId1.toString()). //
-            setTerm(1). //
-            setPrevLogIndex(10). //
-            setPrevLogTerm(1). //
-            setCommittedIndex(0).build();
-        return request;
-    }
-
-    private RpcRequests.AppendEntriesRequest createEmptyEntriesRequestToPeer2() {
-        final RpcRequests.AppendEntriesRequest request = RpcRequests.AppendEntriesRequest.newBuilder(). //
-            setGroupId("test"). //
-            setServerId(new PeerId("localhost", 8081).toString()). //
-            setPeerId(this.peerId2.toString()). //
-            setTerm(1). //
-            setPrevLogIndex(10). //
-            setPrevLogTerm(1). //
-            setCommittedIndex(0).build();
-        return request;
-    }
-
-    private RpcRequests.AppendEntriesRequest createEmptyEntriesRequestToPeer3() {
-        final RpcRequests.AppendEntriesRequest request = RpcRequests.AppendEntriesRequest.newBuilder(). //
-            setGroupId("test"). //
-            setServerId(new PeerId("localhost", 8081).toString()). //
-            setPeerId(this.peerId3.toString()). //
-            setTerm(1). //
-            setPrevLogIndex(10). //
-            setPrevLogTerm(1). //
-            setCommittedIndex(0).build();
-        return request;
+    private RpcRequests.AppendEntriesRequest createEmptyEntriesRequestToPeer(final PeerId peerId) {
+        return RpcRequests.AppendEntriesRequest.newBuilder() //
+            .setGroupId("test") //
+            .setServerId(new PeerId("localhost", 8081).toString()) //
+            .setPeerId(peerId.toString()) //
+            .setTerm(1) //
+            .setPrevLogIndex(10) //
+            .setPrevLogTerm(1) //
+            .setCommittedIndex(0) //
+            .build();
     }
 }
