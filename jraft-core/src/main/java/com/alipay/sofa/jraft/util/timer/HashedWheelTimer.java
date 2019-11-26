b90f93a39c871e6620a63ebc3a474dabb7a14c1a
马赛牧 <masaimu@126.com>
2019-11-26 20:48:04 +0800

    Remove unused imports (#356)
    * Remove unused imports
    
    * fix unit test
    
    * fix unit test

--- jraft-core/src/main/java/com/alipay/sofa/jraft/util/timer/HashedWheelTimer.java
+++ jraft-core/src/main/java/com/alipay/sofa/jraft/util/timer/HashedWheelTimer.java
@@ -32,8 +32,6 @@ import java.util.concurrent.atomic.AtomicLong;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.alipay.sofa.jraft.util.Platform;
-
 /**
  * <h3>Implementation Details</h3>
  * <p>
