11e940613685146baa9c78cd27e00a3ffc95583c
block <jiachun_fjc@163.com>
2020-07-03 19:14:23 +0800

    new bits util (#477)

--- jraft-core/src/main/java/com/alipay/sofa/jraft/util/Bits.java
+++ jraft-core/src/main/java/com/alipay/sofa/jraft/util/Bits.java
@@ -1,85 +1,50 @@
 /*
- * Copyright (c) 2001, 2010, Oracle and/or its affiliates. All rights reserved.
- * ORACLE PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
  *
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package com.alipay.sofa.jraft.util;
 
 /**
- * Moved from java.io.Bits
+ * Bits util.
  *
  * @author boyan (boyan@alibaba-inc.com)
+ * @author jiachun.fjc
  */
 public class Bits {
-    public static int getInt(byte[] b, int off) {
-        return (b[off + 3] & 0xFF) + ((b[off + 2] & 0xFF) << 8) + ((b[off + 1] & 0xFF) << 16) + (b[off] << 24);
+
+    public static int getInt(final byte[] b, final int off) {
+        return HeapByteBufUtil.getInt(b, off);
     }
 
-    public static float getFloat(byte[] b, int off) {
-        return Float.intBitsToFloat(getInt(b, off));
+    public static long getLong(final byte[] b, final int off) {
+        return HeapByteBufUtil.getLong(b, off);
     }
 
-    public static long getLong(byte[] b, int off) {
-        return (b[off + 7] & 0xFFL) + ((b[off + 6] & 0xFFL) << 8) + ((b[off + 5] & 0xFFL) << 16)
-               + ((b[off + 4] & 0xFFL) << 24) + ((b[off + 3] & 0xFFL) << 32) + ((b[off + 2] & 0xFFL) << 40)
-               + ((b[off + 1] & 0xFFL) << 48) + ((long) b[off] << 56);
+    public static void putInt(final byte[] b, final int off, final int val) {
+        HeapByteBufUtil.setInt(b, off, val);
     }
 
-    public static double getDouble(byte[] b, int off) {
-        return Double.longBitsToDouble(getLong(b, off));
+    public static void putShort(final byte[] b, final int off, final short val) {
+        HeapByteBufUtil.setShort(b, off, val);
     }
 
-    public static void putInt(byte[] b, int off, int val) {
-        b[off + 3] = (byte) val;
-        b[off + 2] = (byte) (val >>> 8);
-        b[off + 1] = (byte) (val >>> 16);
-        b[off] = (byte) (val >>> 24);
+    public static short getShort(final byte[] b, final int off) {
+        return HeapByteBufUtil.getShort(b, off);
     }
 
-    public static void putShort(byte[] b, int off, short val) {
-        b[off + 1] = (byte) val;
-        b[off] = (byte) (val >>> 8);
-    }
-
-    public static short getShort(byte[] b, int off) {
-        return (short) ((b[off + 1] & 0xFF) + (b[off] << 8));
-    }
-
-    public static void putFloat(byte[] b, int off, float val) {
-        putInt(b, off, Float.floatToIntBits(val));
-    }
-
-    public static void putLong(byte[] b, int off, long val) {
-        b[off + 7] = (byte) val;
-        b[off + 6] = (byte) (val >>> 8);
-        b[off + 5] = (byte) (val >>> 16);
-        b[off + 4] = (byte) (val >>> 24);
-        b[off + 3] = (byte) (val >>> 32);
-        b[off + 2] = (byte) (val >>> 40);
-        b[off + 1] = (byte) (val >>> 48);
-        b[off] = (byte) (val >>> 56);
-    }
-
-    public static void putDouble(byte[] b, int off, double val) {
-        putLong(b, off, Double.doubleToLongBits(val));
+    public static void putLong(final byte[] b, final int off, final long val) {
+        HeapByteBufUtil.setLong(b, off, val);
     }
 }
