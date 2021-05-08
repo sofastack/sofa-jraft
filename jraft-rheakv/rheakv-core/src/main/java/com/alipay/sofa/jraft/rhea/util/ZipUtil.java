7346c4c439a895544a0c30a987713fbf4ab3ae14
ruqinhu <542849951@qq.com>
2021-05-08 17:55:18 +0800

    Compress/Decompress snapshot in parallel

--- jraft-rheakv/rheakv-core/src/main/java/com/alipay/sofa/jraft/rhea/util/ZipUtil.java
+++ jraft-rheakv/rheakv-core/src/main/java/com/alipay/sofa/jraft/rhea/util/ZipUtil.java
@@ -26,10 +26,11 @@ import java.nio.file.Paths;
 import java.util.zip.CheckedInputStream;
 import java.util.zip.CheckedOutputStream;
 import java.util.zip.Checksum;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipInputStream;
-import java.util.zip.ZipOutputStream;
 
+import org.apache.commons.compress.archivers.ArchiveEntry;
+import org.apache.commons.compress.archivers.zip.ZipArchiveEntry;
+import org.apache.commons.compress.archivers.zip.ZipArchiveInputStream;
+import org.apache.commons.compress.archivers.zip.ZipArchiveOutputStream;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.io.output.NullOutputStream;
@@ -46,26 +47,25 @@ public final class ZipUtil {
                                 final Checksum checksum) throws IOException {
         try (final FileOutputStream fos = new FileOutputStream(outputFile);
                 final CheckedOutputStream cos = new CheckedOutputStream(fos, checksum);
-                final ZipOutputStream zos = new ZipOutputStream(new BufferedOutputStream(cos))) {
-            ZipUtil.compressDirectoryToZipFile(rootDir, sourceDir, zos);
-            zos.flush();
-            fos.getFD().sync();
+                final ZipArchiveOutputStream zaos = new ZipArchiveOutputStream(new BufferedOutputStream(cos))) {
+            ZipUtil.compressDirectoryToZipFile(rootDir, sourceDir, zaos);
+            zaos.closeArchiveEntry();
         }
     }
 
     private static void compressDirectoryToZipFile(final String rootDir, final String sourceDir,
-                                                   final ZipOutputStream zos) throws IOException {
+                                                   final ZipArchiveOutputStream zaos) throws IOException {
         final String dir = Paths.get(rootDir, sourceDir).toString();
         final File[] files = Requires.requireNonNull(new File(dir).listFiles(), "files");
         for (final File file : files) {
             final String child = Paths.get(sourceDir, file.getName()).toString();
             if (file.isDirectory()) {
-                compressDirectoryToZipFile(rootDir, child, zos);
+                compressDirectoryToZipFile(rootDir, child, zaos);
             } else {
-                zos.putNextEntry(new ZipEntry(child));
+                zaos.putArchiveEntry(new ZipArchiveEntry(child));
                 try (final FileInputStream fis = new FileInputStream(file);
-                        final BufferedInputStream bis = new BufferedInputStream(fis)) {
-                    IOUtils.copy(bis, zos);
+                     final BufferedInputStream bis = new BufferedInputStream(fis)) {
+                    IOUtils.copy(bis, zaos);
                 }
             }
         }
@@ -74,16 +74,16 @@ public final class ZipUtil {
     public static void decompress(final String sourceFile, final String outputDir, final Checksum checksum)
                                                                                                            throws IOException {
         try (final FileInputStream fis = new FileInputStream(sourceFile);
-                final CheckedInputStream cis = new CheckedInputStream(fis, checksum);
-                final ZipInputStream zis = new ZipInputStream(new BufferedInputStream(cis))) {
-            ZipEntry entry;
-            while ((entry = zis.getNextEntry()) != null) {
+             final CheckedInputStream cis = new CheckedInputStream(fis, checksum);
+             final ZipArchiveInputStream zais = new ZipArchiveInputStream(new BufferedInputStream(cis))) {
+            ArchiveEntry entry;
+            while ((entry = zais.getNextEntry()) != null) {
                 final String fileName = entry.getName();
                 final File entryFile = new File(Paths.get(outputDir, fileName).toString());
                 FileUtils.forceMkdir(entryFile.getParentFile());
                 try (final FileOutputStream fos = new FileOutputStream(entryFile);
-                        final BufferedOutputStream bos = new BufferedOutputStream(fos)) {
-                    IOUtils.copy(zis, bos);
+                     final BufferedOutputStream bos = new BufferedOutputStream(fos)) {
+                    IOUtils.copy(zais, bos);
                     bos.flush();
                     fos.getFD().sync();
                 }
