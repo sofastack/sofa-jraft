4a4fb023f4901ff6173e8088d4a41a53ec083841
hzh0425 <642256541@qq.com>
2021-06-29 21:49:04 +0800

    1.already do: add OffsetIndex's lifecycle function add OffsetIndex's test
    2.next stage:
    add OffsetIndex's fileHeader if necessary
    add OffsetIndex into SegmentFile

--- jraft-core/src/main/java/com/alipay/sofa/jraft/storage/index/OffsetIndex.java
+++ jraft-core/src/main/java/com/alipay/sofa/jraft/storage/index/OffsetIndex.java
@@ -16,9 +16,15 @@
  */
 package com.alipay.sofa.jraft.storage.index;
 
+import com.alipay.sofa.jraft.storage.log.LibC;
 import com.alipay.sofa.jraft.util.Requires;
+import com.alipay.sofa.jraft.util.Utils;
+import com.sun.jna.NativeLong;
+import com.sun.jna.Pointer;
+import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import sun.nio.ch.DirectBuffer;
 
 import java.io.File;
 import java.io.RandomAccessFile;
@@ -48,6 +54,8 @@ public class OffsetIndex {
 
     private final File          file;
 
+    private final int           maxFileSize;
+
     // mmap byte buffer.
     private MappedByteBuffer    buffer;
 
@@ -69,15 +77,16 @@ public class OffsetIndex {
 
     private final IndexEntry    EMPTY_ENTRY   = new IndexEntry(-1, -1);
 
-    public OffsetIndex(final String path, final Long baseOffset, final int maxSize) {
+    public OffsetIndex(final String path, final Long baseOffset, final int maxFileSize) {
         this.path = path;
+        this.maxFileSize = maxFileSize;
         this.file = new File(path);
         // Init mmap buffer
         try {
             final boolean newlyCreated = file.createNewFile();
             try (final RandomAccessFile raf = new RandomAccessFile(path, "rw")) {
                 if (newlyCreated) {
-                    raf.setLength(maxSize);
+                    raf.setLength(maxFileSize);
                 }
                 this.length = raf.length();
                 this.buffer = raf.getChannel().map(FileChannel.MapMode.READ_WRITE, 0, this.length);
@@ -87,7 +96,7 @@ public class OffsetIndex {
                     // If this file is existed , set position to last index entry
                     this.buffer.position(roundDownToExactMultiple(this.buffer.limit(), this.entrySize));
                 }
-                LOG.info("init a index file, entries: {}", this.entries);
+                LOG.info("init an index file, entries: {}", this.entries);
             }
         } catch (final Throwable t) {
             LOG.error("Fail to init index file {}.", this.path, t);
@@ -95,6 +104,7 @@ public class OffsetIndex {
         this.baseOffset = baseOffset;
         this.maxEntries = this.buffer.limit() / this.entrySize;
         this.entries = this.buffer.position() / this.entrySize;
+        this.largestOffset = this.baseOffset + lastEntry().offset;
     }
 
     /**
@@ -118,6 +128,7 @@ public class OffsetIndex {
         public int getPosition() {
             return position;
         }
+
     }
 
     /**
@@ -126,8 +137,8 @@ public class OffsetIndex {
      * @param position physical position
      */
     public void appendIndex(final Long offset, final int position) {
-        Requires.requireTrue(!isFull(), "Exceeds the maximum index entry number of the index file : {}", this.path);
-        Requires.requireTrue(offset > this.largestOffset, "The append offset {} is no larger than the last offset {}",
+        Requires.requireTrue(!isFull(), "Exceeds the maximum index entry number of the index file : %s", this.path);
+        Requires.requireTrue(offset > this.largestOffset, "The append offset %d is no larger than the last offset %d",
             offset, this.largestOffset);
         this.writeLock.lock();
         try {
@@ -139,7 +150,7 @@ public class OffsetIndex {
             this.entries += 1;
             this.largestOffset = offset;
             Requires.requireTrue(checkPosition(),
-                "Incorrect index position of index file {} , the entries is {} , but the file position is {} ",
+                "Incorrect index position of index file %s , the entries is %d , but the file position is %d ",
                 this.path, this.entries, this.buffer.position());
         } finally {
             this.writeLock.unlock();
@@ -165,6 +176,18 @@ public class OffsetIndex {
             }
         } finally {
             this.readLock.unlock();
+        }
+    }
+
+    /**
+     * Flush data to disk
+     */
+    public void flush() {
+        this.writeLock.lock();
+        try {
+            buffer.force();
+        } finally {
+            this.writeLock.unlock();
         }
     }
 
@@ -201,7 +224,99 @@ public class OffsetIndex {
         try {
             this.entries = slot;
             this.buffer.position(slot * entrySize);
-            this.largestOffset = this.baseOffset + relativeOffset(this.buffer, slot - 1);
+            this.largestOffset = this.baseOffset + lastEntry().offset;
+        } finally {
+            this.writeLock.unlock();
+        }
+    }
+
+    /**
+     * Reset index file
+     */
+    public void reset() {
+        this.writeLock.lock();
+        try {
+            truncateToEntries(0);
+            resize(this.maxFileSize);
+        } finally {
+            this.writeLock.unlock();
+        }
+    }
+
+    /**
+     * Destroy index file.
+     */
+    public void destroy() {
+        this.writeLock.lock();
+        try {
+            if (this.buffer == null) {
+                return;
+            }
+            Utils.unmap(this.buffer);
+            FileUtils.deleteQuietly(new File(this.path));
+            LOG.info("Deleted segment file {}.", this.path);
+        } finally {
+            this.writeLock.unlock();
+        }
+    }
+
+    /**
+     * Shutdown index file
+     */
+    public void shutdown() {
+        this.writeLock.lock();
+        try {
+            if (this.buffer == null) {
+                return;
+            }
+            trimToValidSize();
+            Utils.unmap(this.buffer);
+            this.buffer = null;
+            LOG.info("Unloaded offsetIndex file {}, current status: {}.", this.path, toString());
+        } finally {
+            this.writeLock.unlock();
+        }
+    }
+
+    /**
+     * Trim this index file to fit just the valid entries
+     */
+    private void trimToValidSize() {
+        this.writeLock.lock();
+        try {
+            resize(this.entrySize * this.entries);
+        } finally {
+            this.writeLock.unlock();
+        }
+    }
+
+    /**
+     * Resize file size
+     * only be used in two ways:
+     * 1.trimToValidSize()
+     * 2.reset()
+     */
+    private boolean resize(final int newSize) {
+        this.writeLock.lock();
+        try {
+            final int roundedNewSize = roundDownToExactMultiple(newSize, entrySize);
+            if (this.length == roundedNewSize) {
+                return false;
+            } else {
+                try (final RandomAccessFile raf = new RandomAccessFile(path, "rw")) {
+                    final int prePosition = this.buffer.position();
+                    Utils.unmap(this.buffer);
+                    raf.setLength(roundedNewSize);
+                    this.length = raf.length();
+                    this.buffer = raf.getChannel().map(FileChannel.MapMode.READ_WRITE, 0, this.length);
+                    this.buffer.position(prePosition);
+                    this.maxEntries = this.buffer.limit() / entrySize;
+                    LOG.info("resize an index file, entries: {}", this.entries);
+                } catch (final Throwable t) {
+                    LOG.error("Fail to init index file {}.", this.path, t);
+                }
+                return true;
+            }
         } finally {
             this.writeLock.unlock();
         }
@@ -210,7 +325,7 @@ public class OffsetIndex {
     /**
      * The binary search algorithm is used to find the lower bound for the given target.
      */
-    public int lowerBoundBinarySearch(final ByteBuffer buffer, final int begin, final int end, final int target) {
+    private int lowerBoundBinarySearch(final ByteBuffer buffer, final int begin, final int end, final int target) {
         int lo = begin;
         int hi = end;
         while (lo < hi) {
@@ -226,57 +341,66 @@ public class OffsetIndex {
     }
 
     /**
-     * Flush data to disk
-     */
-    public void flush() {
-        this.writeLock.lock();
-        try {
-            buffer.force();
-        } finally {
-            this.writeLock.unlock();
-        }
-    }
-
-    /**
      * Parse an index entry from this index file
      */
-    public IndexEntry parseEntry(final ByteBuffer buffer, final int n) {
+    private IndexEntry parseEntry(final ByteBuffer buffer, final int n) {
         return new IndexEntry(relativeOffset(buffer, n), physical(buffer, n));
     }
 
     /**
      * Return the relative offset
      */
-    public int relativeOffset(final Long offset) {
+    private int relativeOffset(final Long offset) {
         return (int) (offset - this.baseOffset);
     }
 
     /**
      * Return the relative offset of index entry n
      */
-    public int relativeOffset(final ByteBuffer buffer, final int n) {
+    private int relativeOffset(final ByteBuffer buffer, final int n) {
         return buffer.getInt(n * entrySize);
     }
 
     /**
      * Return the physical position of index entry n
      */
-    public int physical(final ByteBuffer buffer, final int n) {
+    private int physical(final ByteBuffer buffer, final int n) {
         return buffer.getInt(n * entrySize + 4);
     }
 
     /**
      * Round a number to the greatest exact multiple of the given factor less than the given number.
      */
-    public int roundDownToExactMultiple(final int number, final int factor) {
+    private int roundDownToExactMultiple(final int number, final int factor) {
         return factor * (number / factor);
+    }
+
+    /**
+     * Return last entry in this index file
+     */
+    private IndexEntry lastEntry() {
+        if (this.entries >= 1) {
+            return parseEntry(this.buffer, this.entries - 1);
+        } else {
+            return EMPTY_ENTRY;
+        }
     }
 
     public boolean isFull() {
         return entries >= maxEntries;
     }
 
-    public boolean checkPosition() {
+    private boolean checkPosition() {
         return entries * entrySize == buffer.position();
+    }
+
+    public Long getLargestOffset() {
+        return largestOffset;
+    }
+
+    @Override
+    public String toString() {
+        return "OffsetIndex{" + "entrySize=" + entrySize + ", length=" + length + ", entries=" + entries
+               + ", maxEntries=" + maxEntries + ", baseOffset=" + baseOffset + '}';
     }
 }
---
+++ jraft-core/src/test/java/com/alipay/sofa/jraft/storage/index/OffsetIndexTest.java
@@ -0,0 +1,109 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.alipay.sofa.jraft.storage.index;
+
+import com.alipay.sofa.jraft.storage.BaseStorageTest;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.File;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * @author hzh
+ */
+public class OffsetIndexTest extends BaseStorageTest {
+    private static final int  FILE_SIZE   = 8 * 10;
+    private static final Long BASE_OFFSET = 0L;
+    private OffsetIndex       offsetIndex;
+
+    @Before
+    @Override
+    public void setup() throws Exception {
+        super.setup();
+        final String filePath = this.path + File.separator + "OffsetIndexTest";
+        this.offsetIndex = new OffsetIndex(filePath, BASE_OFFSET, FILE_SIZE);
+    }
+
+    @After
+    public void teardown() throws Exception {
+        this.offsetIndex.shutdown();
+        super.teardown();
+    }
+
+    private final OffsetIndex.IndexEntry appendEntry0 = new OffsetIndex.IndexEntry(1, 1);
+    private final OffsetIndex.IndexEntry appendEntry1 = new OffsetIndex.IndexEntry(21, 2);
+    private final OffsetIndex.IndexEntry appendEntry2 = new OffsetIndex.IndexEntry(40, 3);
+
+    @Test
+    public void testAppend() {
+        this.offsetIndex.appendIndex((long) appendEntry0.getOffset(), appendEntry0.getPosition());
+        this.offsetIndex.appendIndex((long) appendEntry1.getOffset(), appendEntry1.getPosition());
+        this.offsetIndex.appendIndex((long) appendEntry2.getOffset(), appendEntry2.getPosition());
+        this.offsetIndex.flush();
+    }
+
+    @Test
+    public void testLookUp() {
+        this.testAppend();
+
+        // Test whether lower bound binary search is true
+        OffsetIndex.IndexEntry entry0 = this.offsetIndex.looUp((long) appendEntry0.getOffset());
+        assertEquals(appendEntry0.getOffset(), entry0.getOffset());
+        OffsetIndex.IndexEntry entry1 = this.offsetIndex.looUp(22L);
+        assertEquals(appendEntry1.getOffset(), entry1.getOffset());
+        OffsetIndex.IndexEntry entry2 = this.offsetIndex.looUp(45L);
+        assertEquals(appendEntry2.getOffset(), entry2.getOffset());
+    }
+
+    @Test
+    public void testReOpen() throws Exception {
+        this.testAppend();
+
+        this.offsetIndex.shutdown();
+
+        // Reopen
+        final String filePath = this.path + File.separator + "OffsetIndexTest";
+        this.offsetIndex = new OffsetIndex(filePath, BASE_OFFSET, FILE_SIZE);
+
+        // Test look up
+        OffsetIndex.IndexEntry entry = this.offsetIndex.looUp((long) appendEntry1.getOffset());
+        assertEquals(appendEntry1.getOffset(), entry.getOffset());
+    }
+
+    @Test
+    public void testTruncate() {
+        // Append 10 index entry
+        for (int idx = 1; idx <= 10; idx++) {
+            this.offsetIndex.appendIndex((long) idx, idx);
+        }
+
+        // Check truncate to 9
+        this.offsetIndex.truncate(9L);
+        assertTrue(this.offsetIndex.getLargestOffset() == 8L);
+        this.offsetIndex.appendIndex(9L, 9);
+
+        // Check truncate to 5
+        this.offsetIndex.truncate(5L);
+        assertTrue(this.offsetIndex.getLargestOffset() == 4L);
+    }
+
+}
