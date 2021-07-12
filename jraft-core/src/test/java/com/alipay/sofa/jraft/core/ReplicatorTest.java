4ba89f69f5ad6d49430f658041631ee68c847055
horizonzy <1060026287@qq.com>
2021-07-12 17:33:08 +0800

    The replicator metrics data add probeCounter, ut need suit it.

--- jraft-core/src/test/java/com/alipay/sofa/jraft/core/ReplicatorTest.java
+++ jraft-core/src/test/java/com/alipay/sofa/jraft/core/ReplicatorTest.java
@@ -169,7 +169,7 @@ public class ReplicatorTest {
         assertNotNull(r);
         assertSame(r.getOpts(), this.opts);
         Set<String> metrics = this.opts.getNode().getNodeMetrics().getMetricRegistry().getNames();
-        assertEquals(6, metrics.size());
+        assertEquals(7, metrics.size());
         r.destroy();
         metrics = this.opts.getNode().getNodeMetrics().getMetricRegistry().getNames();
         assertEquals(1, metrics.size());
