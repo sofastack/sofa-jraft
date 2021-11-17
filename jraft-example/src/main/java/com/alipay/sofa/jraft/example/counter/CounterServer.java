962a4ce0e4376508e1877a961d3070c7de54a14e
zhoupc <zhoupc1994@gmail.com>
2021-11-17 17:14:39 +0800

    (fix) typo

--- jraft-example/src/main/java/com/alipay/sofa/jraft/example/counter/CounterServer.java
+++ jraft-example/src/main/java/com/alipay/sofa/jraft/example/counter/CounterServer.java
@@ -103,7 +103,7 @@ public class CounterServer {
     public static void main(final String[] args) throws IOException {
         if (args.length != 4) {
             System.out
-                .println("Useage : java com.alipay.sofa.jraft.example.counter.CounterServer {dataPath} {groupId} {serverId} {initConf}");
+                .println("Usage : java com.alipay.sofa.jraft.example.counter.CounterServer {dataPath} {groupId} {serverId} {initConf}");
             System.out
                 .println("Example: java com.alipay.sofa.jraft.example.counter.CounterServer /tmp/server1 counter 127.0.0.1:8081 127.0.0.1:8081,127.0.0.1:8082,127.0.0.1:8083");
             System.exit(1);
