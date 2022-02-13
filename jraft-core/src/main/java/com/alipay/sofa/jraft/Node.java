a1248dc3edf78d06ab1aab15bfc4c7de7c13cfc8
OneSizeFitQuorum <tanxinyu@apache.org>
2022-02-13 16:22:37 +0800

    fix typo
    Signed-off-by: OneSizeFitQuorum <tanxinyu@apache.org>

--- jraft-core/src/main/java/com/alipay/sofa/jraft/Node.java
+++ jraft-core/src/main/java/com/alipay/sofa/jraft/Node.java
@@ -211,7 +211,7 @@ public interface Node extends Lifecycle<NodeOptions>, Describer {
      * dead and you'd like to revive the service in the consideration of
      * availability.
      * Notice that neither consistency nor consensus are guaranteed in this
-     * case, BE CAREFULE when dealing with this method.
+     * case, BE CAREFUL when dealing with this method.
      *
      * @param newPeers new peers
      */
