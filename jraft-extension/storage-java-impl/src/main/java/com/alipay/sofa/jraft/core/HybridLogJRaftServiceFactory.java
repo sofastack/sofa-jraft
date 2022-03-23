270a515a2e8c78e923def15a4322037bce12e944
hzh0425 <642256541@qq.com>
2022-03-23 14:50:41 +0800

    style: review

--- jraft-extension/storage-java-impl/src/main/java/com/alipay/sofa/jraft/core/HybridLogJRaftServiceFactory.java
+++ jraft-extension/storage-java-impl/src/main/java/com/alipay/sofa/jraft/core/HybridLogJRaftServiceFactory.java
@@ -16,21 +16,45 @@
  */
 package com.alipay.sofa.jraft.core;
 
+import com.alipay.sofa.jraft.JRaftServiceFactory;
+import com.alipay.sofa.jraft.entity.codec.LogEntryCodecFactory;
+import com.alipay.sofa.jraft.entity.codec.v2.LogEntryV2CodecFactory;
 import com.alipay.sofa.jraft.storage.HybridLogStorage;
 import com.alipay.sofa.jraft.option.RaftOptions;
 import com.alipay.sofa.jraft.option.StoreOptions;
 import com.alipay.sofa.jraft.storage.LogStorage;
+import com.alipay.sofa.jraft.storage.RaftMetaStorage;
+import com.alipay.sofa.jraft.storage.SnapshotStorage;
+import com.alipay.sofa.jraft.storage.impl.LocalRaftMetaStorage;
+import com.alipay.sofa.jraft.storage.snapshot.local.LocalSnapshotStorage;
+import com.alipay.sofa.jraft.util.Requires;
 import com.alipay.sofa.jraft.util.SPI;
+import org.apache.commons.lang.StringUtils;
 
 /**
  * Extends from DefaultJRaftServiceFactory, Overwrite createLogStorage() to create a logitLogStorage
  * @author hzh (642256541@qq.com)
  */
 @SPI(priority = 1)
-public class HybridLogJRaftServiceFactory extends DefaultJRaftServiceFactory {
+public class HybridLogJRaftServiceFactory implements JRaftServiceFactory {
 
     @Override
     public LogStorage createLogStorage(final String uri, final RaftOptions raftOptions) {
+        Requires.requireTrue(StringUtils.isNotBlank(uri), "Blank log storage uri.");
         return new HybridLogStorage(uri, raftOptions, new StoreOptions());
+    }
+
+    public SnapshotStorage createSnapshotStorage(String uri, RaftOptions raftOptions) {
+        Requires.requireTrue(!StringUtils.isBlank(uri), "Blank snapshot storage uri.");
+        return new LocalSnapshotStorage(uri, raftOptions);
+    }
+
+    public RaftMetaStorage createRaftMetaStorage(String uri, RaftOptions raftOptions) {
+        Requires.requireTrue(!StringUtils.isBlank(uri), "Blank raft meta storage uri.");
+        return new LocalRaftMetaStorage(uri, raftOptions);
+    }
+
+    public LogEntryCodecFactory createLogEntryCodecFactory() {
+        return LogEntryV2CodecFactory.getInstance();
     }
 }
