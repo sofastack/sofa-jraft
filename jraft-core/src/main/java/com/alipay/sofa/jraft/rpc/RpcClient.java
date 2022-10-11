68fb475a78b69e2ee8d425c928a2eeb546102f90
lai hui <1353307710@qq.com>
2022-10-11 12:35:42 +0800

    fix:fix code format

--- jraft-core/src/main/java/com/alipay/sofa/jraft/rpc/RpcClient.java
+++ jraft-core/src/main/java/com/alipay/sofa/jraft/rpc/RpcClient.java
@@ -31,109 +31,110 @@ import io.grpc.stub.StreamObserver;
  */
 public interface RpcClient extends Lifecycle<RpcOptions> {
 
-	/**
-	 * Check connection for given address.
-	 *
-	 * @param endpoint target address
-	 * @return true if there is a connection and the connection is active and writable.
-	 */
-	boolean checkConnection(final Endpoint endpoint);
+    /**
+     * Check connection for given address.
+     *
+     * @param endpoint target address
+     * @return true if there is a connection and the connection is active and writable.
+     */
+    boolean checkConnection(final Endpoint endpoint);
 
-	/**
-	 * Check connection for given address and async to create a new one if there is no connection.
-	 * @param endpoint       target address
-	 * @param createIfAbsent create a new one if there is no connection
-	 * @return true if there is a connection and the connection is active and writable.
-	 */
-	boolean checkConnection(final Endpoint endpoint, final boolean createIfAbsent);
+    /**
+     * Check connection for given address and async to create a new one if there is no connection.
+     * @param endpoint       target address
+     * @param createIfAbsent create a new one if there is no connection
+     * @return true if there is a connection and the connection is active and writable.
+     */
+    boolean checkConnection(final Endpoint endpoint, final boolean createIfAbsent);
 
-	/**
-	 * Close all connections of a address.
-	 *
-	 * @param endpoint target address
-	 */
-	void closeConnection(final Endpoint endpoint);
+    /**
+     * Close all connections of a address.
+     *
+     * @param endpoint target address
+     */
+    void closeConnection(final Endpoint endpoint);
 
-	/**
-	 * Register a connect event listener for the replicator group.
-	 *
-	 * @param replicatorGroup replicator group
-	 */
-	void registerConnectEventListener(final ReplicatorGroup replicatorGroup);
+    /**
+     * Register a connect event listener for the replicator group.
+     *
+     * @param replicatorGroup replicator group
+     */
+    void registerConnectEventListener(final ReplicatorGroup replicatorGroup);
 
-	/**
-	 * Synchronous invocation.
-	 *
-	 * @param endpoint  target address
-	 * @param request   request object
-	 * @param timeoutMs timeout millisecond
-	 * @return invoke result
-	 */
-	default Object invokeSync(final Endpoint endpoint, final Object request, final long timeoutMs)
-			throws InterruptedException, RemotingException {
-		return invokeSync(endpoint, request, null, timeoutMs);
-	}
+    /**
+     * Synchronous invocation.
+     *
+     * @param endpoint  target address
+     * @param request   request object
+     * @param timeoutMs timeout millisecond
+     * @return invoke result
+     */
+    default Object invokeSync(final Endpoint endpoint, final Object request, final long timeoutMs)
+            throws InterruptedException, RemotingException {
+        return invokeSync(endpoint, request, null, timeoutMs);
+    }
 
-	/**
-	 * Synchronous invocation using a invoke context.
-	 *
-	 * @param endpoint  target address
-	 * @param request   request object
-	 * @param ctx       invoke context
-	 * @param timeoutMs timeout millisecond
-	 * @return invoke result
-	 */
-	Object invokeSync(final Endpoint endpoint, final Object request, final InvokeContext ctx,
-			final long timeoutMs) throws InterruptedException, RemotingException;
+    /**
+     * Synchronous invocation using a invoke context.
+     *
+     * @param endpoint  target address
+     * @param request   request object
+     * @param ctx       invoke context
+     * @param timeoutMs timeout millisecond
+     * @return invoke result
+     */
+    Object invokeSync(final Endpoint endpoint, final Object request, final InvokeContext ctx,
+                      final long timeoutMs) throws InterruptedException, RemotingException;
 
-	/**
-	 * Asynchronous invocation with a callback.
-	 *
-	 * @param endpoint  target address
-	 * @param request   request object
-	 * @param callback  invoke callback
-	 * @param timeoutMs timeout millisecond
-	 */
-	default void invokeAsync(final Endpoint endpoint, final Object request, final InvokeCallback callback,
-			final long timeoutMs) throws InterruptedException, RemotingException {
-		invokeAsync(endpoint, request, null, callback, timeoutMs);
-	}
+    /**
+     * Asynchronous invocation with a callback.
+     *
+     * @param endpoint  target address
+     * @param request   request object
+     * @param callback  invoke callback
+     * @param timeoutMs timeout millisecond
+     */
+    default void invokeAsync(final Endpoint endpoint, final Object request, final InvokeCallback callback,
+                             final long timeoutMs) throws InterruptedException, RemotingException {
+        invokeAsync(endpoint, request, null, callback, timeoutMs);
+    }
 
-	/**
-	 * Asynchronous invocation with a callback.
-	 *
-	 * @param endpoint  target address
-	 * @param request   request object
-	 * @param ctx       invoke context
-	 * @param callback  invoke callback
-	 * @param timeoutMs timeout millisecond
-	 */
-	void invokeAsync(final Endpoint endpoint, final Object request, final InvokeContext ctx, final InvokeCallback callback,
-			final long timeoutMs) throws InterruptedException, RemotingException;
+    /**
+     * Asynchronous invocation with a callback.
+     *
+     * @param endpoint  target address
+     * @param request   request object
+     * @param ctx       invoke context
+     * @param callback  invoke callback
+     * @param timeoutMs timeout millisecond
+     */
+    void invokeAsync(final Endpoint endpoint, final Object request, final InvokeContext ctx, final InvokeCallback callback,
+                     final long timeoutMs) throws InterruptedException, RemotingException;
 
-	/**
-	 * BidiStreaming invocation.
-	 *
-	 * @param endpoint  target address
-	 * @param request   request object
-	 * @param callback  invoke callback
-	 * @param timeoutMs timeout millisecond
-	 * @return request stream observer.
-	 */
-	default StreamObserver<Message> invokeBidiStreaming(final Endpoint endpoint, final Object request, final InvokeCallback callback,
-			final long timeoutMs) {
-		return invokeBidiStreaming(endpoint, request, null, callback, timeoutMs);
-	}
+    /**
+     * Streaming invocation with a callback.
+     *
+     * @param endpoint  target address
+     * @param request   request object
+     * @param callback  invoke callback
+     * @param timeoutMs timeout millisecond
+     * @return request  stream observer.
+     */
+    default StreamObserver<Message> invokeBidiStreaming(final Endpoint endpoint, final Object request, final InvokeCallback callback,
+            final long timeoutMs) {
+        return invokeBidiStreaming(endpoint, request, null, callback, timeoutMs);
+    }
 
-	/**
-	 * BidiStreaming invocation.
-	 *
-	 * @param endpoint  target address
-	 * @param request   request object
-	 * @param callback  invoke callback
-	 * @param timeoutMs timeout millisecond
-	 * @return request stream observer.
-	 */
-	StreamObserver<Message> invokeBidiStreaming(final Endpoint endpoint, final Object request, final InvokeContext ctx, final InvokeCallback callback,
-			final long timeoutMs);
+    /**
+     * BidiStreaming invocation.
+     *
+     * @param endpoint  target address
+     * @param request   request object
+     * @param callback  invoke callback
+     * @param ctx       invoke context
+     * @param timeoutMs timeout millisecond
+     * @return request  stream observer.
+     */
+    StreamObserver<Message> invokeBidiStreaming(final Endpoint endpoint, final Object request, final InvokeContext ctx, final InvokeCallback callback,
+            final long timeoutMs);
 }
