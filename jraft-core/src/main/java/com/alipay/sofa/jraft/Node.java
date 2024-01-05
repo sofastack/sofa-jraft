0f6efbe9e9ef465cbb7f4dc673d75132a3000593
yuanziwei <yuanziwei@xiaohongshu.com>
2024-01-05 20:46:44 +0800

    Fix known issues

--- jraft-core/src/main/java/com/alipay/sofa/jraft/Node.java
+++ jraft-core/src/main/java/com/alipay/sofa/jraft/Node.java
@@ -231,7 +231,6 @@ public interface Node extends Lifecycle<NodeOptions>, Describer {
      */
     void resetFactor(final int readFactor, final int writeFactor, final Closure done);
 
-
     /**
      * Reset the configuration of this node individually, without any replication
      * to other peers before this node becomes the leader. This function is
