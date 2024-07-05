fd257169f5108f16fb115881b48f962ca2d7a526
flaymes <flaymes@outlook.com>
2024-07-05 23:58:15 +0800

    chore: update getTerm calls

--- jraft-extension/bdb-log-storage-impl/src/test/java/com/alipay/sofa/jraft/storage/impl/BaseLogStorageTest.java
+++ jraft-extension/bdb-log-storage-impl/src/test/java/com/alipay/sofa/jraft/storage/impl/BaseLogStorageTest.java
@@ -84,7 +84,6 @@ public abstract class BaseLogStorageTest extends BaseStorageTest {
         assertEquals(1, this.logStorage.getFirstLogIndex());
         assertEquals(0, this.logStorage.getLastLogIndex());
         assertNull(this.logStorage.getEntry(100));
-        assertEquals(0, this.logStorage.getTerm(100));
     }
 
     @Test
@@ -95,17 +94,27 @@ public abstract class BaseLogStorageTest extends BaseStorageTest {
         assertEquals(100, this.logStorage.getFirstLogIndex());
         assertEquals(100, this.logStorage.getLastLogIndex());
         Assert.assertEquals(entry1, this.logStorage.getEntry(100));
-        assertEquals(1, this.logStorage.getTerm(100));
+        LogEntry logEntry1 = this.logStorage.getEntry(100);
+        assertNotNull(logEntry1);
+        assertEquals(entry1, logEntry1);
+        assertEquals(1, logEntry1.getId().getTerm());
 
         final LogEntry entry2 = TestUtils.mockEntry(200, 2);
         assertTrue(this.logStorage.appendEntry(entry2));
 
         assertEquals(100, this.logStorage.getFirstLogIndex());
         assertEquals(200, this.logStorage.getLastLogIndex());
-        Assert.assertEquals(entry1, this.logStorage.getEntry(100));
-        Assert.assertEquals(entry2, this.logStorage.getEntry(200));
-        assertEquals(1, this.logStorage.getTerm(100));
-        assertEquals(2, this.logStorage.getTerm(200));
+
+        logEntry1 = this.logStorage.getEntry(100);
+        final LogEntry logEntry2 = this.logStorage.getEntry(200);
+        assertNotNull(logEntry1);
+        assertNotNull(logEntry2);
+
+        Assert.assertEquals(entry1, logEntry1);
+        Assert.assertEquals(entry2, logEntry2);
+
+        assertEquals(1, logEntry1.getId().getTerm());
+        assertEquals(2, logEntry2.getId().getTerm());
     }
 
     @Test
@@ -147,8 +156,8 @@ public abstract class BaseLogStorageTest extends BaseStorageTest {
         assertEquals(0, this.logStorage.getFirstLogIndex());
         assertEquals(9, this.logStorage.getLastLogIndex());
         for (int i = 0; i < 10; i++) {
-            assertEquals(i, this.logStorage.getTerm(i));
             final LogEntry entry = this.logStorage.getEntry(i);
+            assertEquals(i, entry.getId().getTerm());
             assertNotNull(entry);
             assertEquals(entries.get(i), entry);
         }
@@ -160,7 +169,9 @@ public abstract class BaseLogStorageTest extends BaseStorageTest {
         this.logStorage.reset(5);
         assertEquals(5, this.logStorage.getFirstLogIndex());
         assertEquals(5, this.logStorage.getLastLogIndex());
-        assertEquals(5, this.logStorage.getTerm(5));
+        final LogEntry logEntry = this.logStorage.getEntry(5);
+        assertNotNull(logEntry);
+        assertEquals(5, logEntry.getId().getTerm());
     }
 
     @Test
@@ -181,7 +192,7 @@ public abstract class BaseLogStorageTest extends BaseStorageTest {
     }
 
     @Test
-    public void testAppendMantyLargeEntries() {
+    public void testAppendManyLargeEntries() {
         final long start = Utils.monotonicMs();
         final int totalLogs = 100000;
         final int logSize = 16 * 1024;
