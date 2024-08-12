8e1de6651689e516d955f7ec950d0efc426c406d
释慧利 <gege87417376@qq.com>
2024-08-12 17:54:33 +0800

    fix: refresh leader on disconnect (#1140)

--- jraft-rheakv/rheakv-core/src/main/java/com/alipay/sofa/jraft/rhea/client/pd/DefaultPlacementDriverRpcService.java
+++ jraft-rheakv/rheakv-core/src/main/java/com/alipay/sofa/jraft/rhea/client/pd/DefaultPlacementDriverRpcService.java
@@ -24,8 +24,8 @@ import java.util.concurrent.ThreadPoolExecutor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.alipay.remoting.exception.RemotingException;
 import com.alipay.sofa.jraft.Status;
+import com.alipay.sofa.jraft.error.ConnectionFailureException;
 import com.alipay.sofa.jraft.rhea.client.failover.FailoverClosure;
 import com.alipay.sofa.jraft.rhea.cmd.pd.BaseRequest;
 import com.alipay.sofa.jraft.rhea.cmd.pd.BaseResponse;
@@ -111,12 +111,7 @@ public class DefaultPlacementDriverRpcService implements PlacementDriverRpcServi
                         closure.run(new Status(-1, "RPC failed with address: %s, response: %s", endpoint, response));
                     }
                 } else {
-                    if (err instanceof RemotingException) {
-                        closure.setError(Errors.RPC_CONNECTION_ERROR);
-                        closure.run(new Status(-1, "RPC failed occur exception %s", err.getMessage()));
-                    } else {
-                        closure.failure(err);
-                    }
+                    closure.failure(err);
                 }
             }
 
@@ -128,8 +123,13 @@ public class DefaultPlacementDriverRpcService implements PlacementDriverRpcServi
 
         try {
             this.rpcClient.invokeAsync(endpoint, request, invokeCtx, invokeCallback, this.rpcTimeoutMillis);
-        } catch (final Throwable t) {
-            closure.failure(t);
+        } catch (final Throwable err) {
+            if (err instanceof ConnectionFailureException) {
+                closure.setError(Errors.RPC_CONNECTION_ERROR);
+                closure.run(new Status(-1, "RPC failed occur exception %s", err.getMessage()));
+            } else {
+                closure.failure(err);
+            }
         }
     }
 
