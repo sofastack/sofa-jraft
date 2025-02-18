65e2783ce7d6c8411f4bd6a0adefa671b1dbe20e
aleksandr-paa <199732564+aleksandr-paa@users.noreply.github.com>
2025-02-18 16:10:54 +0300

    up bolt to 1.6.11

--- jraft-core/src/main/java/com/alipay/sofa/jraft/storage/log/RocksDBSegmentLogStorage.java
+++ jraft-core/src/main/java/com/alipay/sofa/jraft/storage/log/RocksDBSegmentLogStorage.java
@@ -42,7 +42,7 @@ import org.rocksdb.RocksDBException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.alipay.sofa.common.profile.StringUtil;
+import com.alipay.sofa.common.utils.StringUtil;
 import com.alipay.sofa.jraft.option.RaftOptions;
 import com.alipay.sofa.jraft.storage.impl.RocksDBLogStorage;
 import com.alipay.sofa.jraft.storage.log.CheckpointFile.Checkpoint;
